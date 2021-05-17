2d1b33a8ceb2a6115e63061fbdd623837b47fafc
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-17 16:26:54 +0200

    test cambio immagine

--- js/script.js
+++ js/script.js
@@ -2,8 +2,25 @@ var app = new Vue ({
     el: "#root",
     data: {
         message: "Hello World!",
-        colorClass: "red",
+        colorClass: "green",
         imgUrl: "https://it.vuejs.org/images/logo.png"
+    },
+    methods: {
+        changeColor: function () {
+            if (this.colorClass == "green") {
+                this.colorClass = "blue";
+            } else {
+                
+            }
+        },
+
+        changeImg: function () {
+            if (this.imgUrl == "https://it.vuejs.org/images/logo.png") {
+                this.imgUrl = "https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg"
+            } else {
+                this.imgUrl = "https://it.vuejs.org/images/logo.png";
+            }
+        } 
     }
 });
 
