de07d35143bf2c6e8161432a56999ad4f7bd94ba
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-17 16:01:47 +0200

    Inserito messaggio in h1 e immagine, test 1 colore

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var app = new Vue ({
-    el: "root",
+    el: "#root",
     data: {
-        message: "Hello World",
+        message: "Hello World!",
+        colorClass: "red",
+        imgUrl: "https://it.vuejs.org/images/logo.png"
     }
 });
+
